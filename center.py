38731e8efd29f05af4fac1d300f55180ca0de227
Matt Jones <matt-biz@mhjones.org>
2009-05-09 16:03:43 -0700

    make a static dir

--- center.py
+++ center.py
@@ -6,7 +6,6 @@ import json
 urls = (
     '/center', 'center',
     '/reset', 'reset',
-	'/test/(.*)', 'test'
 )
 app = web.application(urls, globals())
 cnt = {'x': 0, 'y': 0}
@@ -25,12 +24,6 @@ class center:
 class reset:
 	def GET(self):
 		cnt = {'x': 100, 'y': 100}
-
-class test:
-	def GET(self, obj):
-		with open(obj) as static_file:
-			contents = static_file.read()
-			return contents
 
 if __name__ == "__main__":
 	app.run()
