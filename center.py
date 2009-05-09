b41d4c3938e16f69fc5ff764e6a5e357c1448c55
Matt Jones <matt-biz@mhjones.org>
2009-05-09 16:25:39 -0700

    move center on mouse click

--- center.py
+++ center.py
@@ -6,6 +6,7 @@ import json
 urls = (
     '/center', 'center',
     '/reset', 'reset',
+	'/click', 'click'
 )
 app = web.application(urls, globals())
 cnt = {'x': 0, 'y': 0}
@@ -15,15 +16,20 @@ class center:
 		cnt['x'] += 25
 		cnt['y'] += 25
 		
-		if cnt['x'] > 750:
-			cnt['x'] = 0
-			cnt['y'] = 0
+		cnt['x'] = cnt['x'] % 750
+		cnt['y'] = cnt['y'] % 750
 
 		return json.dumps(cnt)
 		
 class reset:
 	def GET(self):
 		cnt = {'x': 100, 'y': 100}
+
+class click:
+	def GET(self):
+		form = web.input(x=0, y=0)
+		cnt['x'] = int(form.x)
+		cnt['y'] = int(form.y)
 
 if __name__ == "__main__":
 	app.run()
