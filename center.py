1707f73998548d57166ad2539241d53e0b18fb45
Matt Jones <matt-biz@mhjones.org>
2009-05-09 22:52:03 -0700

    mouse clicks affect velocity + add another thing at mouse click location w/ random velocity

--- center.py
+++ center.py
@@ -11,21 +11,22 @@ urls = (
 )
 app = web.application(urls, globals())
 
-cnt = {'x': 0, 'y': 0}
-delta = {'x': 10, 'y': 10}
+cnts = [{'x': 0, 'y': 0}]
+deltas = [{'x': 10, 'y': 10}]
 
 class center:
 	def GET(self):
-		cnt['x'] += delta['x']
-		cnt['y'] += delta['y']
+		for cnt, delta in zip(cnts, deltas):
+			cnt['x'] += delta['x']
+			cnt['y'] += delta['y']
 
-		delta['x'] = max(-10, min(10, random.randint(-5, 5) + delta['x']))
-		delta['y'] = max(-10, min(10, random.randint(-5, 5) + delta['y']))
+			delta['x'] = max(-10, min(10, random.randint(-5, 5) + delta['x']))
+			delta['y'] = max(-10, min(10, random.randint(-5, 5) + delta['y']))
 
-		cnt['x'] = cnt['x'] % 750
-		cnt['y'] = cnt['y'] % 750
+			cnt['x'] = cnt['x'] % 750
+			cnt['y'] = cnt['y'] % 750
 
-		return json.dumps(cnt)
+		return json.dumps(cnts)
 
 class reset:
 	def GET(self):
@@ -34,8 +35,13 @@ class reset:
 class click:
 	def GET(self):
 		form = web.input(x=0, y=0)
-		delta['x'] = max(-10, min(10, int(form.x) - cnt['x']))
-		delta['y'] = max(-10, min(10, int(form.y) - cnt['y']))
+
+		for cnt, delta in zip(cnts, deltas):
+			delta['x'] = max(-10, min(10, int(form.x) - cnt['x']))
+			delta['y'] = max(-10, min(10, int(form.y) - cnt['y']))
+
+		cnts.append({'x': int(form.x), 'y': int(form.y)})
+		deltas.append({'x': random.randint(-10, 10), 'y': random.randint(-10, 10)})
 
 if __name__ == "__main__":
 	app.run()
